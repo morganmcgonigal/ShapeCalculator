9750e597092d83b700f6147e927d11ef76b3d183
Morgan McGonigal <46797795+morganmcgonigal@users.noreply.github.com>
2019-01-25 13:18:48 -0600

    Add files via upload

--- ShapeCalculator/src/com/company/Main.java
+++ ShapeCalculator/src/com/company/Main.java
@@ -34,37 +34,55 @@ public class Main {
                 break;
             default:
                 System.out.println("Invalid shape.");
+                System.out.println("Please enter a letter to calculate the area and perimeter of a shape.");
+                shape = input.next().charAt(0);
         }
 
     }
 
-    private static double circleCalculator(double cRadius){
+    private static void circleCalculator(double cRadius){
 
+        circleCircumference(cRadius);
+        circleArea(cRadius);
+    }
+
+    private static void circleCircumference (double cRadius){
         double circumference = 2 * (3.14 * cRadius);
         System.out.println("The circumference is: " + circumference);
+    }
+
+    private static void circleArea (double cRadius){
         double area = 2 * (3.14 * Math.pow(cRadius, 2));
         System.out.println("The area is: " + area);
-        return circumference;
     }
 
-    private static double squareCalculator(double sSideA, double sSideB){
+    private static void squareCalculator(double sSideA, double sSideB){
+        squarePerimeter(sSideA, sSideB);
+        squareArea(sSideA, sSideB);
+    }
 
+    private static void squarePerimeter (double sSideA, double sSideB){
         double perimeter = (2 * sSideA) + (2 * sSideB);
         System.out.println("The perimeter is: " + perimeter);
-        double area = sSideA * sSideB;
-        System.out.println("The area is: " + area);
-
-        return perimeter;
-
     }
 
-    private static double rectangleCalculator(double rSideA, double rSideB){
+    private static void squareArea (double sSideA, double sSideB){
+        double area = sSideA * sSideB;
+        System.out.println("The area is: " + area);
+    }
 
+    private static void rectangleCalculator(double rSideA, double rSideB){
+        rectanglePerimeter(rSideA, rSideB);
+        rectangleArea(rSideA, rSideB);
+    }
+
+    private static void rectanglePerimeter (double rSideA, double rSideB){
         double perimeter = (2 * rSideA) + (2 * rSideB);
         System.out.println("The perimeter is: " + perimeter);
+    }
+
+    private static void rectangleArea (double rSideA, double rSideB){
         double area = rSideA * rSideB;
         System.out.println("The area is: " + area);
-
-        return perimeter;
     }
 }
